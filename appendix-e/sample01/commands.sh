b0d9ca4d773b82dab65e4a40d83cae2ef341d1d5
RuijieKang <ruijie.kang@gmail.com>
2024-01-04 10:58:56 -0500

    add dev containers

---
+++ appendix-e/sample01/commands.sh
@@ -0,0 +1,2 @@
+curl -v -X POST --basic -u applicationid:applicationsecret -H "Content-Type: application/x-www-form-urlencoded;charset=UTF-8" -k -d "grant_type=password&username=peter&password=peter123&scope=bar" https://localhost:8443/oauth/token
+
